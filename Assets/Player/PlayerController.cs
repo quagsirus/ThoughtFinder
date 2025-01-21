a6f5a1c1da41035e8aada895e4ac5e460cd4cafc
quagsirus <quagsirus@catpowered.net>
2025-01-21 19:13:53 +0000

    c# v10

--- Assets/Player/PlayerController.cs
+++ Assets/Player/PlayerController.cs
@@ -1,5 +1,7 @@
 using UnityEngine;
 
+namespace Player;
+
 public class PlayerController : MonoBehaviour
 {
     // Start is called once before the first execution of Update after the MonoBehaviour is created
