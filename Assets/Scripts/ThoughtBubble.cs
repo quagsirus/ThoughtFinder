7e01df91c34bcb3b31981500158bef93589747a8
Jake Edwards <jakeedwards238@gmail.com>
2025-01-21 23:57:28 +0000

    speech bubble scale

--- Assets/Scripts/ThoughtBubble.cs
+++ Assets/Scripts/ThoughtBubble.cs
@@ -8,8 +8,10 @@ public class ThoughtBubble : MonoBehaviour
 {
     private PlayerController _playerController;
     private string yapText = "";
+    private bool revealingText = false;
+    private Coroutine _revealCoroutine;
+    [SerializeField] private TMP_Text textTMP;
     [SerializeField] private GameObject text;
-    [SerializeField] private GameObject farawayText;
     [SerializeField] private int displayDistance = 20;
     void Start()
     {
@@ -18,22 +20,42 @@ public class ThoughtBubble : MonoBehaviour
     
     void Update()
     {
-        bool condition = Vector3.Distance(transform.position, _playerController.transform.position) < displayDistance;
-        text.SetActive(condition);
-        farawayText.SetActive(!condition);
+        if (Vector3.Distance(transform.position, _playerController.transform.position) < displayDistance)
+        {
+            if (!revealingText)
+            {
+                revealingText = true;
+                transform.localScale = new Vector3(0.1f, 1, 0.1f);
+                _revealCoroutine = StartCoroutine(RevealText());
+            }
+        }
+        else
+        {
+            if (_revealCoroutine != null) StopCoroutine(_revealCoroutine);
+            transform.localScale = new Vector3(0.1f, 1, 0.1f);
+            revealingText = false;
+            textTMP.text = "...";
+        }
         transform.LookAt(new Vector3(_playerController.transform.position.x, transform.position.y, _playerController.transform.position.z));
     }
 
     private IEnumerator RevealText()
     {
-        foreach (char c in text.GetComponent<TMP_Text>().text)
+        while (transform.localScale.magnitude < Vector3.one.magnitude)
         {
+            transform.localScale += new Vector3(1, 0, 1) * Time.deltaTime;
             yield return null;
+        }
+        textTMP.text = "";
+        foreach (char c in yapText)
+        {
+            textTMP.text += c.ToString();
+            yield return new WaitForSeconds(0.2f);
         }
     }
     
     public void SetYap(string yap)
     {
-        text.GetComponent<TMP_Text>().text = yap;
+        yapText = yap;
     }
 }
