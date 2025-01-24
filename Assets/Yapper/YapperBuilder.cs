66df834db54ed3b6590284013e02baef8248b7a4
quagsirus <quagsirus@catpowered.net>
2025-01-24 16:41:14 +0000

    parent hair and hat to headbone

--- Assets/Yapper/YapperBuilder.cs
+++ Assets/Yapper/YapperBuilder.cs
@@ -1,31 +1,36 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class YapperBuilder : MonoBehaviour
+namespace Yapper
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    [SerializeField] private SkinnedMeshRenderer faceRenderer;
-    [SerializeField] private SkinnedMeshRenderer tShirtRenderer;
-    [SerializeField] private SkinnedMeshRenderer bodyRenderer;
-    [SerializeField] private Transform headBone;
-    [SerializeField] private Material highlights;
-    void Start()
+    public class YapperBuilder : MonoBehaviour
     {
+        // Start is called once before the first execution of Update after the MonoBehaviour is created
+        [SerializeField] private SkinnedMeshRenderer faceRenderer;
+        [SerializeField] private SkinnedMeshRenderer tShirtRenderer;
+        [SerializeField] private SkinnedMeshRenderer bodyRenderer;
+        [SerializeField] private Transform headBone;
+        [SerializeField] private Material highlights;
+        void Start()
+        {
         
-    }
+        }
 
-    // Update is called once per frame
-    void Update()
-    {
+        // Update is called once per frame
+        void Update()
+        {
         
-    }
+        }
 
-    public void BuildYapper(Material face, Material shirt, Material shoes, Material skin, GameObject hair, GameObject hat)
-    {
-        faceRenderer.SetMaterials(new List<Material>(){ face });
-        tShirtRenderer.SetMaterials(new List<Material>(){ shirt });
-        bodyRenderer.SetMaterials(new List<Material>() {shoes, highlights, skin});
-        Instantiate(hair, transform.position, transform.rotation);
-        Instantiate(hat, transform.position, transform.rotation);
+        public void BuildYapper(Material face, Material shirt, Material shoes, Material skin, GameObject hair, GameObject hat)
+        {
+            faceRenderer.SetMaterials(new List<Material>(){ face });
+            tShirtRenderer.SetMaterials(new List<Material>(){ shirt });
+            bodyRenderer.SetMaterials(new List<Material>() {shoes, highlights, skin});
+            var hairInstance = Instantiate(hair, transform.position, transform.rotation);
+            hairInstance.transform.parent = headBone;
+            var hatInstance = Instantiate(hat, transform.position, transform.rotation);
+            hatInstance.transform.parent = headBone;
+        }
     }
 }
